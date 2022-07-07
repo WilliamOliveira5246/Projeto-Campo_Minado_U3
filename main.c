dcab2cadbf46f280ab1c9025036c658a865193e5
William Oliveira <william.oliveira@esig.com.br>
2022-07-07 17:59:10 -0300

    adiciona iniciar board, adiciona iniciar bombas-incomp

--- main.c
+++ main.c
@@ -1,72 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-char** init_board(int row, int col){
-    char ** board = (char **) malloc(row*sizeof(char*));
-    for(int i=0; i<row; i++){
-        board[i] = (char *) malloc(col*sizeof(char));
-        memset(board[i], ' ', col*sizeof(char));
+    //1 - 8
+    // board[0][0] = 1;
+    // board[0][1] = 2;
+    // board[0][2] = 3;
+    // board[0][3] = 4;
+    // board[0][4] = 5;
+    // board[0][5] = 22;
+    // board[0][6] = 15; //bomba
+    // board[0][7] = 42;
+
+typedef struct {
+    char face[3];
+    int status;
+}houses;
+
+
+void setBombs(int row, int col, int qtdBombs, houses * pt_board){
+    for (int b = 0; b<qtdBombs; b++){
+        //int slot = rand() % row*col;
+        int slot = b;
+        if (pt_board[slot].face[1] != 15){
+            pt_board[slot].face[1] = 15;
+        }
+        else b--;
+    }         
+}
+
+
+houses* init_board(int row, int col, int qtdBombs){
+    houses * board = (houses*) malloc(row*col*sizeof(houses));
+    for(int i = 0; i < row;i++){
+        for(int j = 0; j < col;j++){
+            board[i*col+j].face[0] = '#';
+            board[i*col+j].face[1] = '#';//
+            board[i*col+j].status = 0;
+        }
     }
-    //Necessita inserir código de bombas aleatorias
+    setBombs(row,col,qtdBombs,board);
     return board;
 }
 
-void print_board(int row, int col, char ** board){
+
+ void print_board(int row, int col, houses * board){
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
-            printf("| %c ",board[i][j]);
+            printf("| %c ",board[i*col+j].face[1]);
         }
         printf("|\n");
-    }
-}
+     }
+ }
+
 
 int main(){
 
     int row = 10;
     int col = 20;
-    int op = 0;
+    int qtdBombs = 40; //confirmar
+    int op = 0; 
     int fim = 0;
     
-    char ** board = NULL;
-    board = init_board(row,col);
+    houses * board = NULL;
+    //srand((unsigned int)time(NULL));
     
-    board[0][0] = 1;
-    board[0][1] = 2;
-    board[0][2] = 3;
-    board[0][3] = 4;
-    board[0][4] = 5;
-    board[0][5] = 22;
-
-    while (fim == 0){
-        print_board(row, col, board);
-        printf("\nMenu: \n1 - Revelar\n2 - Tempo\n3 - Ajuda\n4 - Fim\n\n");
-        scanf(" %d",&op);
-        switch ( op ){
-            case 1 :
-            printf("revelar");
-            break;
-            
-            case 2 :
-            printf("tempo");
-            break;
-
-            case 3 :
-            printf("ajuda");
-            break;
-
-            case 4 :
-            printf("fim");
-            fim = 1;
-            break;
-
-            default :
-            printf("erro");
-            break;
-        }
-        printf("\n");
-        op = 0;
-    }
+    board = init_board(row,col,qtdBombs);
+    print_board(row, col, board);
     
+    free(board);
     return 0;
 }
